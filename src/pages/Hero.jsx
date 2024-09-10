6922b00218fa45f2a21e0606524541a12f4432a4
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-09-11 02:57:35 +0600

    added vercel.json

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -19,18 +19,23 @@ const Hero = () => {
                 </div>
                 <section className="mt-24 mx-auto max-w-screen-xl pb-12  items-center lg:flex md:px-8">
                     <div className="space-y-4 flex-1 sm:text-center lg:text-left">
-                        <h1 className="text-white font-bold text-4xl xl:text-5xl">
+                        <h1 className="text-white font-bold text-5xl xl:text-6xl">
                             Your tasks more organized
                             <span className="text-[#9FE88D]"> more precise</span>
                         </h1>
                         <p className="text-gray-300 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum
+                        Experience a streamlined workflow with our task management system.
                         </p>
                     </div>
                     <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
                         <img src={gbook} className="w-full mx-auto sm:w-10/12  lg:w-full" />
                     </div>
                 </section>
+                <div className='flex justify-evenly bg-[#9FE88D] text-gray-900 rounded-sm h-20 text-lg md:text-xl font-bold items-center'>
+                    <p>Create Account</p>
+                    <p>Add Task</p>
+                    <p>Save Automatically</p>
+                </div>
             </div>
         </div>
     );
